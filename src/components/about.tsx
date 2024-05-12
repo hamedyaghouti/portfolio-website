fa4c240cfe2bea42b17129eaf08017810f8e5695
h.yaghooti <hamedyaghooti1992@gmail.com>
2024-05-12 21:01:14 +0330

    handle work section

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -16,17 +16,24 @@ const ServiceCard = ({
   icon: string;
 }) => {
   return (
-    <Tilt className="xs:w-[250px] w-full">
+    <Tilt
+      className="xs:w-[250px] w-full"
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450,
+      }}
+    >
       <motion.div
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
       >
         <div
-          options={{
-            max: 45,
-            scale: 1,
-            speed: 450,
-          }}
+          // options={{
+          //   max: 45,
+          //   scale: 1,
+          //   speed: 450,
+          // }}
           className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
         >
           <img src={icon} alt={title} className="w-16 h-16 object-contain" />
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,13 +1,14 @@
 import About from "./about";
 import Experience from "./experience";
 import Tech from "./tech";
+import Works from "./works";
 
 export * from "./contact";
 export * from "./feedbacks";
 export * from "./hero";
 export * from "./navbar";
 export * from "./stars-canvas";
-export * from "./works";
+// export * from "./works";
 export * from "./canvas-loader";
 
-export { About, Experience, Tech };
+export { About, Experience, Tech, Works };
